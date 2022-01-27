8daca66a089616e4779f6d852469a21a77fff27b
VAFAGIT <belgazzarwafae@gmail.com>
2022-01-27 14:23:18 +0100

    v3

---
+++ inventory_management (1).sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 27 jan. 2022 à 14:21
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `inventory_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products`
+--
+
+CREATE TABLE `products` (
+  `id_product` int(11) NOT NULL,
+  `P_name` varchar(20) DEFAULT NULL,
+  `Quantity` int(11) DEFAULT NULL,
+  `Category` varchar(10) DEFAULT NULL,
+  `Price` decimal(10,2) DEFAULT NULL,
+  `image_url` varchar(300) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id_product`, `P_name`, `Quantity`, `Category`, `Price`, `image_url`, `id_user`) VALUES
+(75, 'chemise', 12, 'Tops', '123.00', '61efc8a78bf7e9.96972688prct.webp', 1),
+(80, 'SWEAT EN MAILLE', 39, 'Tops', '30.00', 'ptr1.png', 2),
+(82, 'hsgsf', 12, 'panta', '123.00', '61f1de86418435.44126146prct.webp', 1),
+(83, 'nava', 145, 'boutine', '55.00', '61f1e689d75232.49256700bg.jpg', 1),
+(84, 'pull black', 123, 'Tops', '29.00', '61f275b26f4296.33569650prct.webp', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `F_name` varchar(10) DEFAULT NULL,
+  `L_name` varchar(10) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Password` varchar(30) DEFAULT NULL,
+  `profile_img` varchar(105) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `F_name`, `L_name`, `Email`, `Password`, `profile_img`) VALUES
+(1, 'wafae', 'Belgazzar', 'wafaebel@gmail.com', 'wafae123', 'user1.PNG'),
+(2, 'Rachid', 'Naim', 'Rachid@gmail.com', 'rachid123', 'user2.jpg');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
